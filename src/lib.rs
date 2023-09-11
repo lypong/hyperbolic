837a3c9c964b4bc62f45eea3ab1c74f04cca3db3
seb <sfv.jacob@gmail.com>
2023-09-11 18:25:10 +0200

    Implement reflect for Line

--- src/lib.rs
+++ src/lib.rs
@@ -63,15 +63,22 @@ impl Line {
         let orthogonal_direction = Vec2::new(-self.direction.y,self.direction.x);
         Line { start: point, end: point+orthogonal_direction, direction: orthogonal_direction }
     }
-    pub fn intersect(&self,line : Self) -> Point2{
+    pub fn intersect(&self,line : Self) -> Result<Point2,()>{
         let factor = (line.direction.x*(self.start.y-line.start.y)+line.direction.y*(line.start.x-self.start.x))/(line.direction.y*self.direction.x-line.direction.x*self.direction.y);
-        Point2::new(self.start.x+factor*self.direction.x,self.start.y+factor*self.direction.y)
+        if !factor.is_finite(){
+            return Err(());
+        }
+        Ok(Point2::new(self.start.x+factor*self.direction.x,self.start.y+factor*self.direction.y))
+    }
+    pub fn projection(&self,point: Point2) -> Point2{
+        let orthogonal = self.orthogonal_line_passing_by_point(point);
+        self.intersect(orthogonal).unwrap()
     }
 }
 
 impl Reflect for Line {
     fn reflect(&self, point: Point2) -> Point2 {
-        let projection = self.orthogonal_line_passing_by_point(point).intersect(*self);
+        let projection = self.projection(point);
         let direction = projection-point;
         Point2::new(point.x+direction.x*2f32, point.y+direction.y*2f32)
     }
